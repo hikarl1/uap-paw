4c09ccc83f7b722e7c30cf2fe0aa1b814355482e
hikarl1 <83234547+hikarl1@users.noreply.github.com>
2025-11-21 15:52:14 +0700

    Add files via upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+        'title' => 'required|max:255',
+        'body' => 'required',
+        ]);
+        Post::create($request->all());
+        return redirect()->route('posts.index')
+        ->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+    return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+      'title' => 'required|max:255',
+      'body' => 'required',
+    ]);
+    $post = Post::find($id);
+    $post->update($request->all());
+    return redirect()->route('posts.index')
+      ->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * 
+     */
+     public function create()
+    {
+        return view('posts.create');
+    }
+    /**
+     * Show the form for editing the specified resource.        
+     *  
+     */
+        public function edit($id)
+    {
+        $post = Post::find($id);
+        return view('posts.edit', compact('post'));
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+    $post->delete();
+    return redirect()->route('posts.index')
+      ->with('success', 'Post deleted successfully');
+    }
+}
